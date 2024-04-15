642fc0be2fc1b35564e232d2af83d9a7176d965c
Neethu-Puzhakkal <159087664+Neethu-Puzhakkal@users.noreply.github.com>
2024-04-14 20:53:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ except socket.gaierror:
 
 @app.route('/')
 def hello_cloud():
-  return "Welcome to Neethu Puzhakkal's Final Test API Server"
+  return "Welcome to Neethu Puzhakkal's API Server"
   
 @app.route('/host')
 def host_name():
